4b2b02751556bb51d6770a36c9642f2c23947239
Juan José Bazán Espinosa <eltiburonjuanjo@hotmail.com>
2021-11-02 13:22:34 +0100

    tras comentar y arreglar algunos bugs e incosistencias, finalice formalmente la parte A

--- src/main/java/ejercicios_ut03_estructuras_repeticion/paresnones/ParesNonesMachine.java
+++ src/main/java/ejercicios_ut03_estructuras_repeticion/paresnones/ParesNonesMachine.java
@@ -5,11 +5,11 @@ import java.util.Random;
 
 public class ParesNonesMachine {
     // entrada manual para jugar a pares o nones
-
+    // repositorio público: https://github.com/Naturalkidv1/ejercicioParesNonesJJ.git
     public static void main(String[] args) {
 
         final int DEDOS_MAX = 10;
-        final int DEDOS_MIN = 1;
+        final int DEDOS_MIN = 0;
 
         String decisionJ1;
         String decisionJ2;
@@ -21,12 +21,18 @@ public class ParesNonesMachine {
         
         Scanner entradaTeclado = new Scanner(System.in);
 
+        // tras importar el Scanner y el Random, declarar e inicializar las 
+        // constantes y declarar las variables e inicializar el método Scanner, 
+        // comenzaremos a realizar ambas partes para anexionarlas con un switch
+        
+        
         // esto sería el modulo A 
         System.out.println("Buenas Jugador 1 y Jugador 2, las reglas son simples \n"
                 + "cada jugador sacará tantos dedos como quiera después de decir si el número resultante será par o impar, \n"
                 + "luego, se sumaran. \n"
                 + "Quien acierte en su decisión, gana.");
 
+        // empezamos, con los controles de seguridad y la inicialización de las variables 
         do {
 
             System.out.println("Jugador 1, ¿pares o impares? Escriba su respuesta como 'pares' o 'nones', por favor. ");
@@ -35,7 +41,7 @@ public class ParesNonesMachine {
 
         do {
             System.out.println("Y, ¿cuántos dedos va a sacar?\n"
-                    +"Recuerde que mínimo puede sacar un dedo y máximo diez dedos.");
+                    +"Recuerde que puede sacar el puño vacío y máximo diez dedos.");
             dedoJ1 = entradaTeclado.nextInt();
         } while (!(dedoJ1 >= DEDOS_MIN && dedoJ1 <= DEDOS_MAX));
 
@@ -50,10 +56,13 @@ public class ParesNonesMachine {
         
         do {
             System.out.println("¿Cuántos dedos sacará?\n"
-                    + "Recuerde que mínimo puede sacar un dedo y máximo diez dedos.");
+                    + "Recuerde que puede sacar el puño vacío y máximo diez dedos.");
             dedoJ2 = entradaTeclado.nextInt();
         } while (!(dedoJ2 >= DEDOS_MIN && dedoJ2 <= DEDOS_MAX));
 
+        // procederemos a la parte final donde se indicaran las elecciones de cada jugador
+        // y el resultado del encuentro
+        
         System.out.println("El jugador 1 ha elegido " + decisionJ1);
         System.out.println("Y ha sacado " + dedoJ1 + " dedo(s).");
         System.out.println("El jugador 2 elige " + decisionJ2);
@@ -72,7 +81,7 @@ public class ParesNonesMachine {
         System.out.println("Y " + dedoTotal + " dedos, es " + paridadTotal + ".");
         
 
-        if (paridadTotal == decisionJ1) {
+        if (paridadTotal.equals(decisionJ1)) {
             System.out.println("El Jugador 1 ha ganado.");  
         } else {
             System.out.println("El Jugador 2 ha ganado.");
